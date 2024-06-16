0d4afa8126f5058a6cceb8683d1c815dbdc61d0c
squaredb <squaredb@outlook.com>
2024-06-16 18:33:37 +0200

    Configured Clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { ClerkProvider } from "@clerk/nextjs";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -15,16 +17,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <head>
-        <link rel="icon" href={process.env.NEXT_PUBLIC_METADATA_LOGO} sizes="any" />
-        <link rel="apple-touch-icon" href={process.env.NEXT_PUBLIC_METADATA_APPLE_TOUCH_ICON} sizes="any"/>
-      </head>
-      <body className={inter.className}>
-        <main>
-          {children}
-        </main>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <head>
+          <link
+            rel="icon"
+            href={process.env.NEXT_PUBLIC_METADATA_LOGO}
+            sizes="any"
+          />
+          <link
+            rel="apple-touch-icon"
+            href={process.env.NEXT_PUBLIC_METADATA_APPLE_TOUCH_ICON}
+            sizes="any"
+          />
+        </head>
+        <body className={inter.className}>
+          <main>{children}</main>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
