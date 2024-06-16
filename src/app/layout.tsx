0db177e40b0a9571f71c82e6f686380261b194da
squaredb <squaredb@outlook.com>
2024-06-16 18:08:58 +0200

    Init

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: process.env.NEXT_PUBLIC_METADATA_NAME,
+  description: process.env.NEXT_PUBLIC_METADATA_DESCRIPTION,
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href={process.env.NEXT_PUBLIC_METADATA_LOGO} sizes="any" />
+        <link rel="apple-touch-icon" href={process.env.NEXT_PUBLIC_METADATA_APPLE_TOUCH_ICON} sizes="any"/>
+      </head>
+      <body className={inter.className}>
+        <main>
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Hom (){
+  return (
+    <>
+     hello world
+    </>
+  );
+}
