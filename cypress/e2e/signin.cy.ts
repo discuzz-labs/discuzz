0ed0db22d4761ba33574e652a6ba6106c17cf755
solo-fox <work.abdelrahman@outlook.com>
2024-07-14 20:31:01 +0200

    Tested sign up process on failing e2e

--- cypress/e2e/signin.cy.ts
+++ cypress/e2e/signin.cy.ts
@@ -57,4 +57,32 @@ describe("Sign-In Test", () => {
     cy.get('a[href*="sign-up"]').should("exist");
     cy.get('a[href*="sign-in"]').should("exist");
   });
+
+  it("Fills out the sign-in form  with wrong password and getting invalid credentials error", () => {
+    // Visit the site
+    cy.visit("/");
+
+    // Click the sign-up link
+    cy.get('a[href*="sign-in"]').click({ force: true });
+    // Fill out the sign-up form
+    cy.get('input[name="email"]').type("test@example.com");
+    cy.get('input[name="password"]').type("wrongpassword");
+
+    cy.get('button[type="submit"]').contains("Sign In.").click({ force: true });
+
+    cy.get(".cy-alert")
+      .should("be.visible")
+      .contains(ERROR.LOGIN_FAILED_WRONG_CREDENTIALS);
+
+    // Check if localStorage contains the userToken
+    cy.window().then((window) => {
+      const userToken = window.localStorage.getItem("userToken");
+      expect(userToken).to.be.null;
+    });
+
+    // Verify that the two specific links in the navbar have disappeared
+
+    cy.get('a[href*="sign-up"]').should("exist");
+    cy.get('a[href*="sign-in"]').should("exist");
+  });
 });
