31a0c5dc548b4c11ffdeba52bba2442272883ee9
solo-fox <work.abdelrahman@outlook.com>
2024-07-17 17:46:21 +0200

    Updated jest config to solve GA Issue

--- jest.config.ts
+++ jest.config.ts
@@ -70,14 +70,14 @@ const config: Config = {
   // globalTeardown: undefined,
 
   // A set of global variables that need to be available in all test environments
-  // globals: {
-  //   // we must specify a custom tsconfig for tests because we need the typescript transform
-  //   // to transform jsx into js rather than leaving it jsx such as the next build requires.  you
-  //   // can see this setting in tsconfig.jest.json -> "jsx": "react"
-  //   "ts-jest": {
-  //     tsconfig: "tsconfig.jest.json",
-  //   },
-  // },
+  globals: {
+    // we must specify a custom tsconfig for tests because we need the typescript transform
+    // to transform jsx into js rather than leaving it jsx such as the next build requires.  you
+    // can see this setting in tsconfig.jest.json -> "jsx": "react"
+    "ts-jest": {
+      tsconfig: "tsconfig.jest.json",
+    },
+  },
 
   // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
   // maxWorkers: "50%",
@@ -182,11 +182,11 @@ const config: Config = {
   // testRunner: "jest-circus/runner",
 
   // A map from regular expressions to paths to transformers
-  transform: {
-    // Use babel-jest to transpile tests with the next/babel preset
-    // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
-    "^.+\\.(js|jsx|ts|tsx)$": ["ts-jest", { config: "tsconfig.jest.json" }],
-  },
+  // transform: {
+  //   // Use babel-jest to transpile tests with the next/babel preset
+  //   // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
+  //   "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
+  // },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
